a076a460ab67b2be2da0ec279f5e8604bc962d2f
Oshin <happinessxd743@gmail.com>
2024-02-17 14:04:05 +0545

    edited

--- src/primealt.c
+++ src/primealt.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 int main() {
-    int num = 0, rem = 0;
+    int num = 0, rem = 0,sum=0,s,i=0,y,p=0,d;
     char prime='y';
     printf("Enter a number:\n");
     scanf("%d",&num);
-
-    switch (num) {
+    y=num;
+    d=num;
+     switch (num) {
     case 0:
         printf("ZERO");
         break;
@@ -37,12 +38,32 @@ int main() {
         }
         break;
     }
-
+    printf("%d",num);
     if(prime=='y') {
         printf("\033[0;32mPrime\033[0m");
     }
     else {
         printf("\033[0;31mNOT Prime\033[0m");
+    }
+
+    while(num!=0)
+    {
+        num=num/10;
+        i++;
+    }
+    while(y!=0)
+    {
+        s=y%10;
+        int r=pow(s,i);
+        p=p+r;
+        y=y/10;
+    }
+    if(p==d)
+    {
+        printf("it is an armstrong number");
+    }
+    else{
+        printf("it is not an armstrong number");
     }
     getch();
     return 0;
